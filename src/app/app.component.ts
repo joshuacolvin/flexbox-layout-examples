331d5a4f44616bddf755a3acbc4e38fe002bdeab
Joshua Colvin <joshua@joshuacolvin.net>
2021-11-16 09:06:05 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { PeakIconRegistry } from "@ascentgl/peak-icons";
+import { peakIconBookmark } from "@ascentgl/peak-icons/icons";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'flexbox-layout-examples';
+  constructor(private registry: PeakIconRegistry) {
+    this.registry.register(peakIconBookmark);
+  }
+
+  flexDirection = ["row", "row-reverse", "column", "column-reverse"];
+  flexDirectionValue = "row";
+
+  flexWrap = ["nowrap", "wrap", "wrap-reverse"];
+  flexWrapValue = "nowrap";
+
+  justifyContent = [
+    "flex-start",
+    "flex-end",
+    "center",
+    "space-between",
+    "space-around",
+    "space-evenly",
+  ];
+
+  justifyContentValue = "flex-start";
+
+  alignItems = ["flex-start", "flex-end", "center", "stretch", "baseline"];
+  alignItemsValue = "flex-start";
+
+  alignSelf = ["auto", "flex-start", "flex-end", "center", "stretch"];
+  alignSelfValue = "auto";
+
+  flexBasis = ["auto", 0, "200px"];
+  flexBasisValue = "auto";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { PeakButtonModule, PeakCardModule } from "@ascentgl/peak-ui";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { MatExpansionModule } from "@angular/material/expansion";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatSelectModule } from "@angular/material/select";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule } from "@angular/forms";
+import { PeakIconModule } from "@ascentgl/peak-icons";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PeakButtonModule,
+    PeakCardModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatExpansionModule,
+    PeakIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
